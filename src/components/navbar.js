f0e3ed728c83aeff4310b996658f94dd7656ea44
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-12 14:08:21 -0700

    Added bootstrap navbar to navbar component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,11 @@
-import React from "react"
+import React from "react";
 
-function Navbar(props){
-
-    
-
+function Navbar(props) {
+  return (
+    <nav class="navbar navbar-light bg-light">
+      <span class="navbar-brand mb-0 h1">Navbar</span>
+    </nav>
+  );
 }
-
 
 export default Navbar;
