2e6d0a454ac08d1470cb906f8a0866b598c31e49
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-12 14:09:56 -0700

    Navbar styling finalized

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+const Styles = {
+    nav: {
+        fontWeight: 900
+    }
+}
 function Navbar(props) {
   return (
-    <nav class="navbar navbar-light bg-light">
-      <span class="navbar-brand mb-0 h1">Navbar</span>
+    <nav class="navbar navbar-dark bg-dark">
+      <span class="navbar-brand mb-0 h1" style={Styles.nav}>Clicky Game</span>
     </nav>
   );
 }
