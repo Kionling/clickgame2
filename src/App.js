94b3354feb0001e3b340ecba697b977692a96ee0
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-12 14:32:55 -0700

    Added footer.js for footer component. Finalized

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 // import CardDesign from "./components/cardDesign"
-import Navbar from "./components/navbar"
-import Score from "./components/score"
+import Navbar from "./components/navbar";
+import Score from "./components/score";
 // import GameRules from "./components/gamerules"
-import Jumbotron from "./components/jumbotron"
+import Jumbotron from "./components/jumbotron";
 
 function App() {
   return (
     <div>
-    <Jumbotron />
-    <Navbar />
-    <Score />
+      <Navbar />
+      <Jumbotron />
+      <Score />
     </div>
   );
 }
