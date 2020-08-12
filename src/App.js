84b6207abb361572b54788a1720c8a563d257daf
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-12 14:03:46 -0700

    Exporting and importing all components within app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CardDesign from "./components/cardDesign"
+import Navbar from "./components/navbar"
+import Score from "./components/score"
+import GameRules from "./components/gamerules"
 
 function App() {
   return (
