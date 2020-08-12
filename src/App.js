97fbbd99bf62353997a095007a80fdb932fb91f9
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-12 14:07:16 -0700

    Return method was not responding with any component data. Return syntax added to fix issue

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import GameRules from "./components/gamerules"
 
 function App() {
   return (
-    
+    <Score />
   );
 }
 
--- src/components/score.js
+++ src/components/score.js
@@ -1,6 +1,18 @@
-import React from "react"
+import React from "react";
 
-function Score(props){
+const Styles = {
+  Score: {
+    fontWeight: 900,
+  },
+};
 
+function Score(props) {
+  return (
+    <div className="container">
+      <h4 style={Styles.Score}>
+        Score:<span className={Styles.Score}></span>
+      </h4>
+    </div>
+  );
 }
 export default Score;
