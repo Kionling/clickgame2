c223af964594efa0c9a4b5bfc7e550211615de4a
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-12 14:16:24 -0700

    Created jumbotron.js file for jumbotron component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import CardDesign from "./components/cardDesign"
+// import CardDesign from "./components/cardDesign"
 import Navbar from "./components/navbar"
 import Score from "./components/score"
-import GameRules from "./components/gamerules"
+// import GameRules from "./components/gamerules"
 
 function App() {
   return (
+    <div>
     <Navbar />
     <Score />
+    </div>
   );
 }
 
