7bab236be165346960e2fb9b48fbe41acd199d00
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-12 18:58:32 -0700

    Adding gamerules to cards

--- src/components/cardDesign.js
+++ src/components/cardDesign.js
@@ -1,10 +1,10 @@
 import React from "react"
-
+import GameRules from "./gamerules"
 function CardDesign(props){
 return(
     <div className="card">
         <div className="container">
-         <img src={props.image} onClick={() => {}}/>
+         <img src={props.image} onClick={() => {props.random(); props.counter();}}/>
         </div>
     </div>
 );
