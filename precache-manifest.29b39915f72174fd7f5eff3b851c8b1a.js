f45ad8b6e56a67a958642eab91702ed2059ec791
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-12 20:18:52 -0700

    Updates

---
+++ precache-manifest.29b39915f72174fd7f5eff3b851c8b1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d09f1ddacdd45636867f2a29ce9832",
+    "url": "/clickgame2/index.html"
+  },
+  {
+    "revision": "2113608a09687f467046",
+    "url": "/clickgame2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "be62b9ed70b8b014625b",
+    "url": "/clickgame2/static/js/2.479cc52a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clickgame2/static/js/2.479cc52a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2113608a09687f467046",
+    "url": "/clickgame2/static/js/main.62883387.chunk.js"
+  },
+  {
+    "revision": "89c123fe3d617c8f5ced",
+    "url": "/clickgame2/static/js/runtime-main.fc284365.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickgame2/static/media/logo.5d5d9eef.svg"
+  }
+]);
